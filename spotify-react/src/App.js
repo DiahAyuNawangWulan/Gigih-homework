ff03d15e2ffe538c98872853591e23c853603551
DiahAyuNawangWulan <diahayunawangwulan27@gmail.com>
2022-04-04 07:54:45 +0700

    merge pull request

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -1,16 +1,16 @@
 //import './App.css';
 //import SearchPage from './pages/Search';
 import Pages from './pages/index';
-// import Navbar from './components/navbar';
+import Navbar from './components/navbar';
 //import data from './simple/data'
 //import PlayList from './components/playlist';
 //import MusicCard from './components/MusicCard'
 function App() {
   return (
     <div className="App-header">
-       {/*<MusicCard data={data} />*/}
+      {/*<MusicCard data={data} />*/}
       {/*<PlayList data={data}/>*/}
-      {/*<Navbar />*/}
+      <Navbar />
       <Pages />
     </div>
   );
