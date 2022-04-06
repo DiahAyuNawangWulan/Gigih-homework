b2fcafc1139d921c52f6d8f60b1c85aebecf4da5
DiahAyuNawangWulan <diahayunawangwulan27@gmail.com>
2022-04-07 00:21:43 +0700

    Adding redux

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -1,17 +1,10 @@
-//import './App.css';
-//import SearchPage from './pages/Search';
-import Pages from './pages/index';
-import Navbar from './components/navbar';
-//import data from './simple/data'
-//import PlayList from './components/playlist';
-//import MusicCard from './components/MusicCard'
+import './index.css';
+import Home from "./page/home"
 function App() {
   return (
-    <div className="App-header">
-      {/*<MusicCard data={data} />*/}
-      {/*<PlayList data={data}/>*/}
-      <Navbar />
-      <Pages />
+    <div className="App">
+     <Home/>
+
     </div>
   );
 }
