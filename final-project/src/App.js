2c0a29c78912731191cdea20ec7e26da6e074929
DiahAyuNAwangWulan <diahayunawangwulan27@gmail.com>
2022-04-18 07:15:12 +0700

    try to login

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import Login from './components/Login/Login';
+import React, {useEffect, useState} from 'react';
+import { getTokenFromUrl } from './components/Spotify/Spotify';
+import hash from 'string-hash';
 
 function App() {
+const [token, setToken] = useState(null);
+
+  useEffect (() => {
+    const token = getTokenFromUrl ();
+    window.location.hash ='';
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log ("I have a token", token);
+  }, []);
+
   return (
-    <div className="App">
-      
-      <Login/>
-    </div>
+    <div className="App">{token?(<h1>Hi Diah was here</h1>):(<Login/>)}
+      </div>
   );
 }
 
--- final-project/src/components/Spotify/Spotify.js
+++ final-project/src/components/Spotify/Spotify.js
@@ -1,6 +1,6 @@
-export const authEndpoint = 'https://account.spotify.com/authorize';
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
 
-const redirectUri = 'http.//localhost:3000/'
+const redirectUri = 'http://localhost:3000/'
 const clientId = '0acfdb6f18ac49be8df0e0ff3856e405'
 const scopes = [
     "user-read-currently-playing",
@@ -10,4 +10,17 @@ const scopes = [
     "user-modify-playback-state",
 ];
 
-export const LoginUrl = `${authEndpoint}?client.id=${clientId}&redirect_uri=${redirectUri}&scopes=${scopes.join("%20")}&response_type=token&show_dialog=true`
+export const getTokenFromUrl = () => {
+    return window.location.hash
+      .substring(1)
+      .split("&")
+      .reduce((initial, item) => {
+        var parts = item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+  
+        return initial;
+      }, {});
+  };
+
+export const LoginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scopes=${scopes.join("%20")}&response_type=token&show_dialog=true`
+
